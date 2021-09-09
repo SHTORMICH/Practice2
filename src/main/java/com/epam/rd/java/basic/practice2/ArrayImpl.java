6ab8763893ccd29c72fd49ab7509b1960b2bc037
Olexandr <olesukmail@gmail.com>
2021-09-09 12:56:13 +0300

    ArrayImpl

--- src/main/java/com/epam/rd/java/basic/practice2/ArrayImpl.java
+++ src/main/java/com/epam/rd/java/basic/practice2/ArrayImpl.java
@@ -1,17 +1,24 @@
 package com.epam.rd.java.basic.practice2;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class ArrayImpl implements Array {
+    private static final int DEFAULT_CAPACITY = 10;
+    Object[] elementData = new Object[DEFAULT_CAPACITY];
+    private int size = 0;
 
 	@Override
     public void clear() {
-        
+        for (int i = 0; i < size; i++) {
+            elementData[i] = null;
+        }
+        size = 0;
     }
 
 	@Override
     public int size() {
-        return 0;
+        return size;
     }
 	
 	@Override
@@ -20,42 +27,68 @@ public class ArrayImpl implements Array {
     }
 	
 	private class IteratorImpl implements Iterator<Object> {
+        int index;
 
         @Override
         public boolean hasNext() {
+            if (index < size) {
+                return true;
+            }
             return false;
         }
 
         @Override
         public Object next() {
-            return null;
+            return elementData[index];
         }
 
     }
 	
 	@Override
     public void add(Object element) {
-        
+        if (size >= elementData.length) {
+            arrayIncrease();
+        }
+        elementData[size++] = element;
     }
 
 	@Override
     public void set(int index, Object element) {
-        
+	    chekIndex(index);
+	    elementData[index] = element;
     }
 
 	@Override
     public Object get(int index) {
-        return null;
+        chekIndex(index);
+        return elementData[index];
     }
 
 	@Override
     public int indexOf(Object element) {
-        return 0;
+        if (element == null) {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == null)
+                    return i;
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (element.equals(elementData[i]))
+                    return i;
+            }
+        }
+        return -1;
     }
 
 	@Override
     public void remove(int index) {
-        
+        chekIndex(index);
+
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elementData, index + 1, elementData, index, numMoved);
+        }
+        elementData[--size] = null;
     }
 
     @Override
@@ -63,8 +96,16 @@ public class ArrayImpl implements Array {
         return null;
     }
 
-    public static void main(String[] args) {
+    public void arrayIncrease() {
+	    int oldCapacity = elementData.length;
+	    int newCapacity = oldCapacity + (oldCapacity >> 1);
+	    elementData = Arrays.copyOf(elementData, newCapacity);
+    }
 
+    public void chekIndex(int index) {
+	    if (index > size || index < 0) {
+	        throw new IndexOutOfBoundsException();
+        }
     }
 
 }
